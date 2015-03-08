0834a0f49e3580590fb4255c5d3bfef1370c8001
Michael Ryu <michaelryu0@gmail.com>
2015-03-07 16:34:01 -0800

    added editing and deleting bookmarks

--- app/eggly-app.start.js
+++ app/eggly-app.start.js
@@ -1,6 +1,7 @@
 angular.module("app", [])
 
   .controller("MainController", function () {
+
     this.categories = [
       {"id": 0, "name": "Development"},
       {"id": 1, "name": "Design"},
@@ -28,10 +29,14 @@ angular.module("app", [])
       this.currentCategory = category;
       this.cancelCreating();
       this.cancelEditing();
-    }
+    };
     this.isCurrentCategory = function (category) {
       return this.currentCategory !== null && this.currentCategory.name === category.name;
-    }
+    };
+
+    this.isSelectedBookmark = function (bookmark){
+      return this.editedBookmark !== null && this.editedBookmark.id === bookmark.id;
+    };
 
     // Creating and editing states
     this.isCreating = false;
@@ -40,27 +45,29 @@ angular.module("app", [])
     this.startCreating = function () {
       this.isCreating = true;
       this.isEditing = false;
-    }
+    };
 
     this.cancelCreating = function () {
       this.isCreating = false;
-    }
+    };
     this.startEditing = function () {
       this.isCreating = false;
       this.isEditing = true;
       this.resetCreateForm();
-    }
+    };
     this.cancelEditing = function () {
       this.isEditing = false;
-    }
+    };
     this.shouldShowCreating = function () {
       return this.currentCategory && !this.isEditing && this.isCreating;
-    }
+    };
     this.shouldShowEditing = function () {
       return this.currentCategory && !this.isCreating && this.isEditing;
-    }
+    };
 
     // Forms
+
+    this.editedBookmark = null;
 
     this.resetCreateForm = function () {
       this.newBookmark = {
@@ -68,10 +75,30 @@ angular.module("app", [])
         url: '',
         category: this.currentCategory
       }
-    }
+    };
     this.createBookmark = function (bookmark) {
       bookmark.id = this.bookmarks.length;
+      bookmark.category = this.currentCategory.name;
       this.bookmarks.push(bookmark);
       this.resetCreateForm();
-    }
+    };
+
+    this.setEditedBookmark = function (bookmark) {
+      this.editedBookmark = angular.copy(bookmark);
+    };
+
+    this.updateBookmark = function (bookmark) {
+      var index = _.findIndex(this.bookmarks, function (b) {
+        return b.id == bookmark.id;
+      })
+      this.bookmarks[index] = bookmark;
+      this.isEditing = false;
+      this.editedBookmark = null;
+    };
+
+    this.deleteBookmark = function(bookmark){
+      _.remove(this.bookmarks, function(b){
+        return b.id == bookmark.id;
+      })
+    };
   })
